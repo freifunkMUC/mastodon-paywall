9a06ca9d5eaa9b45d7397a28e3fe3f6c3e9b92fc
Christoph Caprano <christoph.caprano@valiton.com>
2023-01-01 18:56:36 +0100

    first formula ui version

---
+++ components/paypalButton.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import {
+	PayPalButtons,
+	usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
+
+export default function PaypalButton({ getFormValues, triggerValidation, registerUser }) {
+	const [{ options }, dispatch] = usePayPalScriptReducer();
+
+	useEffect(() => {
+        dispatch({
+            type: "resetOptions",
+            value: {
+                ...options,
+                intent: "subscription",
+            },
+        });
+    }, []);
+
+	return (
+  
+    <PayPalButtons
+		createSubscription={(data, actions) =>
+			actions.subscription
+				.create({
+					plan_id: process.env.NEXT_PUBLIC_PAYPAL_PLAN_ID,
+				})
+		}
+    onApprove={async (data, actions) => {
+      const subscriptionData = await actions.subscription.get();
+      const formData = getFormValues();
+      const userData = {
+        ...formData,
+        subscriptionId: subscriptionData.subscriptionID,
+        orderId: subscriptionData.orderID,
+      };
+        
+      return registerUser(userData);
+    }}
+    onClick={(data, actions) => triggerValidation().then(isValid => isValid ? actions.resolve() : actions.reject())
+    }
+		style={{
+			label: "subscribe",
+		}}
+	/>);
+}
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from '../components/form'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>FFMuc Registration</title>
+        <meta name="description" content="FFMuc Registration" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Form />
+      </main>
+    </>
+  )
+}
