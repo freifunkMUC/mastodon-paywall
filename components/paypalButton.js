9b48aad550faadd650b663080308711205bd71c1
GoliathLabs <git@xdfr.de>
2025-04-10 22:08:08 +0200

    feat: use @next/env

--- components/paypalButton.js
+++ components/paypalButton.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import './envConfig.ts'
 
 export default function PaypalButton({
   getFormValues,
